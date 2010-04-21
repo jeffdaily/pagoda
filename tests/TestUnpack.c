47b6d3bb9b283413569dffd6920b8f3737ab1487
Jeff Daily <jeff.daily@pnnl.gov>
2010-04-21 19:41:36 +0000

    TestUnpack demonstrates how GA_Unpack is severely broken.
    git-svn-id: svn://localhost/pagoda/trunk@118 803cb72c-d64d-0410-ae51-a9422f8e9f96

--- tests/TestUnpack.c
+++ tests/TestUnpack.c
@@ -14,33 +14,35 @@ int main(int argc, char **argv)
 
     int me = GA_Nodeid();
     int nproc = GA_Nnodes();
-    int size = 10;
+    int size_dst = 10;
     int size_src = 6; // MUST MATCH proc 0 put of mask below
-    int g_src = NGA_Create(MT_INT, 1, &size_src, "src", NULL);
-    int g_dst = NGA_Create(MT_INT, 1, &size, "dst", NULL);
-    int g_msk = NGA_Create(MT_INT, 1, &size, "msk", NULL);
+    //int g_src = NGA_Create(MT_INT, 1, &size_src, "src", NULL);
+    int g_src = NGA_Create(MT_INT, 1, &size_dst, "src", NULL);
+    int g_dst = NGA_Create(MT_INT, 1, &size_dst, "dst", NULL);
+    int g_msk = NGA_Create(MT_INT, 1, &size_dst, "msk", NULL);
     int ONE_NEG = -1;
     int icount;
 
+    GA_Fill(g_src, &ONE_NEG);
     GA_Fill(g_dst, &ONE_NEG);
-    GA_Sync();
     GA_Patch_enum(g_src, 0, size_src-1, 0, 1);
-    GA_Sync();
     if (0 == me) {
         // MUST MATCH size_src count above
-        //int vals[] = {0,0,1,0,1,1,1,0,0,1}; // 10 == size, count = 5
-        int vals[] = {1,0,1,0,1,1,1,0,0,1}; // 10 == size, count = 6
+        //int vals[] = {0,0,1,0,1,1,1,0,0,1}; // 10 == size_dst, count = 5
+        int vals[] = {1,0,1,0,1,1,1,0,0,1}; // 10 == size_dst, count = 6
         int lo = 0;
-        int hi = size-1;
+        int hi = size_dst-1;
 
         NGA_Put(g_msk, &lo, &hi, vals, NULL);
     }
     GA_Sync();
 
-    //GA_Unpack(g_src, g_dst, g_msk, 0, size-1, &icount);
-
     GA_Print(g_src);
     GA_Print(g_msk);
+    GA_Print(g_dst);
+
+    GA_Unpack(g_src, g_dst, g_msk, 0, size_dst-1, &icount);
+
     GA_Print(g_dst);
 
     GA_Terminate();
