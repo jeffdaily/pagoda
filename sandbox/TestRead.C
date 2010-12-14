b7c9f5265527ad3c5106d3056732c2fed20b88f9
Jeff Daily <jeff.daily@pnnl.gov>
2010-12-14 21:31:39 +0000

    TestRead in sandbox reads and compares all variables.
    git-svn-id: svn://localhost/pagoda/trunk@262 803cb72c-d64d-0410-ae51-a9422f8e9f96

--- sandbox/TestRead.C
+++ sandbox/TestRead.C
@@ -18,92 +18,99 @@ using std::endl;
 
 /**
  * 1) Open a dataset.
- * 2) Attempt to read variable into an Array.
+ * 2) Attempt to read a variable or all variables into Array(s).
  * 3) Compare the blocking and non-blocking values.
  */
 int main(int argc, char **argv)
 {
     Dataset *dataset;
-    Variable *variable;
-    Array *array_blocking;
-    Array *array_nonblocking;
-    bool ok = true;
+    vector<Variable*> variables;
+    vector<Array*> arrays;
+    vector<Array*> nbarrays;
 
     pagoda::initialize(&argc, &argv);
 
-    if (3 != argc) {
-        if (0 == pagoda::me) {
-            cerr << "Usage: TestRead filename variablename" << endl;
-        }
+    if (2 != argc && 3 != argc) {
+        pagoda::println_zero("Usage: TestRead filename [variablename]");
         pagoda::finalize();
         return EXIT_FAILURE;
     }
 
     dataset = Dataset::open(argv[1]);
-    variable = dataset->get_var(argv[2]);
+    if (2 == argc) {
+        vector<Variable*> vars = dataset->get_vars();
+        for (size_t i=0; i<vars.size(); ++i) {
+            Variable *var = vars[i];
+            DataType type = var->get_type();
+            if (type == DataType::INT
+                    || type == DataType::FLOAT
+                    || type == DataType::DOUBLE) {
+                variables.push_back(vars[i]);
+            }
+        }
+    } else {
+        variables.push_back(dataset->get_var(argv[2]));
+    }
 
-    // blocking
-    if (0 == pagoda::me) {
-        cerr << "blocking read... ";
+    for (size_t i=0; i<variables.size(); ++i) {
+        Variable *var = variables[i];
+        string name = var->get_name();
+        pagoda::print_zero("blocking read of " + name + " ... ");
+        arrays.push_back(var->read(int64_t(0)));
+        pagoda::println_zero("done");
+        pagoda::print_zero("non-blocking read of " + name + " ... ");
+        nbarrays.push_back(var->iread(int64_t(0)));
+        pagoda::println_zero("done");
     }
-    array_blocking = variable->read(int64_t(0));
-    if (0 == pagoda::me) {
-        cerr << "done" << endl;
-    }
-
-    // non-blocking
-    if (0 == pagoda::me) {
-        cerr << "non-blocking read... ";
-    }
-    array_nonblocking = variable->iread(int64_t(0));
-    if (0 == pagoda::me) {
-        cerr << "done" << endl;
-    }
-    if (0 == pagoda::me) {
-        cerr << "wait... ";
-    }
+    pagoda::print_zero("wait... ");
     dataset->wait();
-    if (0 == pagoda::me) {
-        cerr << "done" << endl;
-    }
+    pagoda::println_zero("done");
 
     // compare the arrays
     // it is assumed they have the same distributions
-    if (array_blocking->owns_data()) {
-        DataType type = array_blocking->get_type();
-        int64_t size = array_blocking->get_local_size();
+    for (size_t i=0; i<arrays.size(); ++i) {
+        Variable *var = variables[i];
+        Array *array = arrays[i];
+        Array *nbarray = nbarrays[i];
+        bool ok = true;
+        string name = var->get_name();
+        if (array->owns_data()) {
+            DataType type = array->get_type();
+            int64_t size = array->get_local_size();
 #define compare_helper(DT,T) \
-        if (type == DT) { \
-            T *b  = (T*)array_blocking->access(); \
-            T *nb = (T*)array_nonblocking->access(); \
-            for (int64_t i=0; i<size; ++i) { \
-                ok &= (b[i] == nb[i]); \
-                if (!ok) { \
-                    break; \
+            if (type == DT) { \
+                T *b  = (T*)array->access(); \
+                T *nb = (T*)nbarray->access(); \
+                for (int64_t i=0; i<size; ++i) { \
+                    ok &= (b[i] == nb[i]); \
+                    if (!ok) { \
+                        break; \
+                    } \
                 } \
-            } \
-        } else
-        compare_helper(DataType::INT, int)
-        compare_helper(DataType::FLOAT, float)
-        compare_helper(DataType::DOUBLE, double)
-        {
-            if (0 == pagoda::me) {
-                cerr << "unhandled data type" << endl;
+            } else
+            compare_helper(DataType::INT, int)
+            compare_helper(DataType::FLOAT, float)
+            compare_helper(DataType::DOUBLE, double)
+            {
+                pagoda::println_zero("unhandled data type");
                 ok = false;
             }
-        }
 #undef compare_helper
-    }
-
-    if (ok) {
-        pagoda::print_sync("ok\n");
-    } else {
-        pagoda::print_sync("failure\n");
+            array->release();
+            nbarray->release();
+        }
+        if (ok) {
+            pagoda::print_sync(name + " ok\n");
+        } else {
+            pagoda::print_sync(name + " failure\n");
+        }
     }
 
     delete dataset;
-    delete array_blocking;
-    delete array_nonblocking;
+    for (size_t i=0; i<arrays.size(); ++i) {
+        delete arrays[i];
+        delete nbarrays[i];
+    }
 
     pagoda::finalize();
 
